ab8b1c686d1997df44d091131e94719001bb9fab
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-07-25 16:42:33 +1200

    Update to v1.3.216 SDK release.

--- teapot.rb
+++ teapot.rb
@@ -4,19 +4,31 @@
 
 teapot_version "3.0"
 
-define_target "vulkan-library" do |target|
-	target.depends "SDK/Vulkan", public: true
-	
-	target.provides "Library/vulkan" do
-		append linkflags "-lvulkan"
-	end
+teapot_version "3.0"
+
+define_project "vulkan-sdk-linux" do |project|
+	project.add_author "Samuel Williams"
+	project.license = " Apache License, Version 2.0 / MIT License"
+
+	project.version = "1.3.216"
 end
 
-define_target 'vulkan-platform-xcb' do |target|
-	target.provides 'Vulkan/Platform/XCB' do
+define_target 'vulkan-sdk-linux-library' do |target|
+	target.provides 'Vulkan/SDK/Linux/Library' do
+		# append header_search_paths(target.package.path + "source")		
+		append linkflags "-lvulkan"
+	end
+
+	target.provides :vulkan_library => 'Vulkan/SDK/Linux/Library'
+end
+
+define_target 'vulkan-sdk-linux-platform-xcb' do |target|
+	target.depends 'Vulkan/SDK/Linux/Library', public: true
+	
+	target.provides 'Vulkan/SDK/Linux/Platform/XCB' do
 		append buildflags "-DVK_USE_PLATFORM_XCB_KHR"
 		append linkflags "-lxcb"
 	end
 	
-	target.provides :vulkan_platform => 'Vulkan/Platform/XCB'
+	target.provides :vulkan_platform => 'Vulkan/SDK/Linux/Platform/XCB'
 end
