73db145d99a99996f9bc9b194a90ddfa4b3ac3e3
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-07-25 17:58:06 +1200

    Remove SDK from package name.

--- teapot.rb
+++ teapot.rb
@@ -6,29 +6,29 @@ teapot_version "3.0"
 
 teapot_version "3.0"
 
-define_project "vulkan-sdk-linux" do |project|
+define_project "vulkan-linux" do |project|
 	project.add_author "Samuel Williams"
 	project.license = " Apache License, Version 2.0 / MIT License"
 
 	project.version = "1.3.216"
 end
 
-define_target 'vulkan-sdk-linux-library' do |target|
-	target.provides 'Vulkan/SDK/Linux/Library' do
+define_target 'vulkan-linux-library' do |target|
+	target.provides 'Library/Vulkan/Linux' do
 		# append header_search_paths(target.package.path + "source")		
 		append linkflags "-lvulkan"
 	end
 
-	target.provides :vulkan_library => 'Vulkan/SDK/Linux/Library'
+	target.provides :vulkan_library => 'Library/Vulkan/Linux'
 end
 
-define_target 'vulkan-sdk-linux-platform-xcb' do |target|
-	target.depends 'Vulkan/SDK/Linux/Library', public: true
+define_target 'vulkan-linux-platform-xcb' do |target|
+	target.depends 'Library/Vulkan/Linux', public: true
 	
-	target.provides 'Vulkan/SDK/Linux/Platform/XCB' do
+	target.provides 'Platform/Vulkan/Linux/XCB' do
 		append buildflags "-DVK_USE_PLATFORM_XCB_KHR"
 		append linkflags "-lxcb"
 	end
 	
-	target.provides :vulkan_platform => 'Vulkan/SDK/Linux/Platform/XCB'
+	target.provides :vulkan_platform => 'Platform/Vulkan/Linux/XCB'
 end
